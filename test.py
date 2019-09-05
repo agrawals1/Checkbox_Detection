f820b6b13ffd2dff08b24c367239fd97d3efba3c
Durgesh <durgeshreddiyar@yahoo.com>
2019-09-05 12:42:28 +0530

    added waitkeys

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-from skimage.filter import threshold_local
+import cv2
+import imutils 
 
+image = cv2.imread("EAS-Formular-1.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = imutils.resize(gray, height = 500)
+cv2.imshow("greyed", gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
